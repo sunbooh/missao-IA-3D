a6be0a0a940fb65915b74bb45a1bcc4ecae2dc19
sunbooh <148074723+sunbooh@users.noreply.github.com>
2024-09-11 10:11:30 -0300

    kk

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,9 @@
-export function aleatorio (lista){
+const nomes = ["juca","jose carlos", "deolane", "catra","pipokinha","alfredo"];
+
+export function aleatorio (lista)
     const posicao = Math.floor(Math.random()* lista.lenght);
     return lista[posicao];
   }
+
+  export const nome = aleatorio(nomes)
+  
