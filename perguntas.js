47391189b972c69a206afe4357e2a35aa66338fa
sunbooh <148074723+sunbooh@users.noreply.github.com>
2024-10-17 08:16:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,99 @@
+export const perguntas = [
+    {
+        enunciado: "O que são mitos?",
+        alternativas: [
+            {
+                texto: "é uma narrativa fantástica que possui o objetivo de explicar a origem de tudo aquilo que existe e é considerado importante para um determinado povo.",
+                afirmacao: [
+                    "De fato povos tradicionais costumavam criar os ditos mitos para explicar tais acontecimentos 'inexplicáveis', pois naqueles tempos a ciência não era um método utilizado frequentemente.",
+                    ],
+                proxima: 1,
+            },
+            {
+                texto: "São estados mentais em que se assume que algo é verdadeiro ou provável. Elas são expressas linguisticamente por meio de afirmações.",
+                afirmacao: [
+                    "Apesar de em algum lugar isso fazer algum sentido, na verdade essa é a definição de uma crença, não de um mito.",
+                    ],
+                proxima: 1,
+            }           
+            
+        ]
+    },
+    {
+        enunciado: "Qual a importância do folclore na cultura brasileira?",
+        alternativas: [
+            {
+                texto:"é um meio de aproximação entre os povos e grupos sociais e de afirmação de sua identidade cultural.",
+                afirmacao: [
+                    "Apesar de não ser uma resposta completamente errada, é bom lembrar de que o folclore foi e ainda é um dos pilares sociais de como toda a sociedade se formou, com milhares crescendo ouvindo tais mitos, talvez manter uma mente mais aberta da próxima aberta?",
+                    ],
+                proxima: 2,
+            },
+            {
+                texto: "Simboliza a essência da cultura popular, atuando como um pilar fundamental na construção da identidade de um povo.",
+                afirmacao: [
+                    "Bom, a forma resumida da resposta é essa ao menos, a forma longa? Apenas pense da forma que você cresceu ouvindo histórias.",
+                   ],
+                proxima: 2,
+            }
+        ]
+    },
+    {
+        enunciado: "Passar essas histórias também têm uma outra função, qual seria?",
+        alternativas: [
+            {
+                texto:"Entender o passado, mantendo uma história e modo de vida presente na história e não esquecido no fluxo do tempo",
+                afirmacao: [
+                    "Muitas coisas se perdem no fluxo do tempo da história sendo assim, continuar contando e escutando essas histórias de muitas formas serve para que essas culturas não sejam esquecidas e que no futuro as pessoas possam compreender o meio de vida dos antepassados. ",
+                    ],
+                proxima: 3,
+            },
+            {
+                texto:"Contar como um aviso para crianças mais novas se comportarem.",
+                afirmacao: [
+                    "Sem sombra de dúvidas essas histórias agem como um meio de aviso, porém não é correto diminuir uma cultura inteira a isso. ",
+                    ],
+                proxima: 3,
+            }
+            
+        ]
+    },
+    {
+        enunciado: "Sabendo que esses mitos e histórias surgiam para explicar um evento, quais eram os eventos mais comumente relacionados a eles?",
+        alternativas: [
+            {
+                texto:"Eventos sociais.",
+                afirmacao: [
+                    "Existem diversos mitos na história que relatam eventos sociais que ocorreram em um passado distante, como por exemplo a odisseia que conta o após o fim da guerra de troia ou até mesmo os mitos da guerra de troia que durou 10 anos, no entanto os mitos não são realmente conhecidos por isso.",
+                    ],
+                proxima: 4,
+            },
+            {
+                texto: "Eventos naturais.",
+                afirmacao: [
+                    "Sim, a maioria dos mitos surge para explicar todas as coisas que a natureza faz, desde o surgimento dos oceanos até o para onde vamos após a morte, normalmente esses mitos colocam um divindade, um ser superior a nós humanos, que nos julga e realiza as punições com base em nossas ações.",
+                    ],
+                proxima: 4,
+            }
+        ]
+    },
+    {
+        enunciado: "No fim de tudo, juntando os conhecimentos adquiridos pelas perguntas e os seus próprios, é correto dizer que essas ideias em um passado, possivelmente em um presente e futuro também, poderiam ser considerados um meio de controle?",
+        alternativas: [
+            {
+                texto:"Sim",
+                afirmacao: [
+                    "A resposta é correta mas apenas meio, não tem como dizer com certeza se sim ou não essas histórias puramente usadas como um meio de controle pois isso dependeria de quem as conta, num passado de fato elas eram um meio de colocar medo nas pessoas para impedi-las de cometerem 'pecados' mas muitas vezes elas serviam apenas como uma explicação de uma ação impossível para acalmar a mente das pessoas, criar um ser divino e superior ao humano é um meio ortodoxo de garantir para um povo leigo que não estamos sozinhos na imensidão do universo e que em algum momento nos encontraríamos com esses seres",
+                    ],
+                proxima: 0,
+            },
+            {
+                texto: "Não",
+                afirmacao: [
+                    "Essa resposta não é errada, mas ela definitivamente vai para um lado mais oposto a correta, os mitos e histórias, para um povo no passado uma religião, agiram sim como um meio de controle populacional pois proporcionava que o povo agisse de certa maneira e evitasse certos atos, tópicos e pensamentos com o medo da punição divina que vinha com isso",
+                    ],
+                    proxima: 0,
+            }
+        ]
+    },
+];
